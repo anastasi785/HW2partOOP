1e79ae991fd89581552b496b53ecefc9191e0462
nastu <nastushik92@bk.ru>
2022-10-20 16:02:45 +0300

    вы

--- src/Human.java
+++ src/Human.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Human {
+public class Human {
+    int yearOfBirth;
+    String name;
+    String city;
+    String job;
+
+    public static int calculateYearOfBirth(int yearOfBirth) {
+        int year = 2022 - yearOfBirth;
+
+        return year;
+    }
+    void speaks() {
+        System.out.println("Привет! Меня зовут "+name+". Я из города "+ city+". Я родился в "+calculateYearOfBirth(yearOfBirth)+" году. Я работаю на должности "+job+". Будем знакомы!");
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
 public class Main {
     public static void main(String[] args) {
+        Human max=new Human();
+        max.name="Максим";
+        max.city="Минск";
+        max.yearOfBirth=35;
+        max.job="бренд-менеджер";
+        max.speaks();
 
+        Human anya=new Human();
+        anya.name="Аня";
+        anya.city="Москва";
+        anya.yearOfBirth=29;
+        anya.job="методист образовательных программ";
+        anya.speaks();
+
+        Human kat=new Human();
+        kat.name="Катя";
+        kat.city="Калининград";
+        kat.yearOfBirth=28;
+        kat.job="продакт-менеджер";
+        kat.speaks();
+
+        Human artem=new Human();
+        artem.name="Артём";
+        artem.city="Москва";
+        artem.yearOfBirth=27;
+        artem.job="директор по развитию бизнеса";
+        artem.speaks();
     }
 }
